c44b0d05dcac32b678c3a1a4283d2f0c82ed61b1
Philipp Zagar <zagar@stanford.edu>
2023-11-09 11:40:30 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,12 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
+
+//
+// This source file is part of the TemplatePackage open source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
 
 import PackageDescription
 
